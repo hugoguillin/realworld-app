77fc87a6d95bb601c601ba9787715306fa2d62c3
Hugo Guillin <hugo5guillin@gmail.com>
2024-02-11 13:00:28 +0100

    Add dockerfile and docker-compose

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Ejecutar el comando para crear la base de datos
+npm run sqlz -- db:create
+
+# Iniciar la aplicación
+npm run dev
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: 'localhost',
     port: 3000,
     proxy: {
       '/api': {
