4e1949757a66a1abc21462dbb923d312ae97daa2
hugoguillin <hugo5guillin@gmail.com>
2024-03-17 20:58:38 +0100

    Add script to push app image to Docker Hub

---
+++ push-image-dh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get env variables to login in Docker Hub
+DOCKER_HUB_USERNAME=$DOCKER_HUB_USERNAME
+DOCKER_HUB_PASSWORD=$DOCKER_HUB_PASSWORD
+
+# Check if the environment variables are set
+if [ -z "$DOCKER_HUB_USERNAME" ] || [ -z "$DOCKER_HUB_PASSWORD" ]; then
+  echo "Error: Environment variables DOCKER_HUB_USERNAME and DOCKER_HUB_PASSWORD are not set."
+  exit 1
+fi
+
+# Compila la imagen
+docker build -t realworldapp-for-test-automation:latest .
+
+# Etiqueta la imagen con el nombre de usuario de Docker Hub
+docker tag realworldapp-for-test-automation:latest $DOCKER_HUB_USERNAME/realworldapp-for-test-automation:latest
+
+# Inicia sesión en Docker Hub
+docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
+
+# Sube la imagen a Docker Hub
+docker push $DOCKER_HUB_USERNAME/realworldapp-for-test-automation:latest
