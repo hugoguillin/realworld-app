ffbe45f84f61cd57fa72c703aeabbb8a2fffb050
hugoguillin <hugo5guillin@gmail.com>
2024-03-17 21:10:41 +0100

    Minor changes

---
+++ backend/seeders/20220427123216-create-users.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const { faker } = require("@faker-js/faker");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = Array(5)
+      .fill(null)
+      .map((_, index) => ({
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        createdAt: faker.date.past(),
+        updatedAt: new Date(),
+      }));
+
+    await queryInterface.bulkInsert("Users", users, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
